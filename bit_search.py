4428ca69715b1bcaae6f835b733f7c1e6fdefcd6
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-10-04 20:13:32 +0700

    fix: edit bit search method

--- bit_search.py
+++ bit_search.py
@@ -8,11 +8,11 @@ def bit_search(fn, a, b, eps):
         if y_0 > y_1:
             x_0 = x_1
             y_0 = y_1
-            if a < x_0 < b: continue
-            elif x_0 >= b:
-                x_0 = b
-            elif x_0 <= a:
-                x_0 = a
+        if a < x_0 < b: continue
+        elif x_0 >= b:
+            x_0 = b
+        elif x_0 <= a:
+            x_0 = a
         if abs(h) <= eps:
             return {'x': x_0, 'y': fn(x_0)}
         else:
